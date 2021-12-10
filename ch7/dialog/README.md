010193196b11275d34a9b549ec8498e9b37e7430
aifrak <aifrak@users.noreply.github.com>
2021-12-10 16:39:00 +0000

    Add `dialog` app from chapter 7.4

---
+++ ch7/dialog/README.md
@@ -0,0 +1,16 @@
+# Dogs app
+
+This is an example app from "7.4 Implementing a dialog component".
+
+# How to run this app?
+
+1. Install NodeJS.
+
+2. Run those commands in your console:
+
+```bash
+npm install
+npm run start:dev
+```
+
+3. Open your browser at `localhost:5000`.
---
+++ ch7/dialog/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Dialog from './Dialog.svelte';
+  let dialog;
+</script>
+
+<div>
+  <button on:click={() => dialog.showModal()}>Open Dialog</button>
+</div>
+
+<Dialog title="Test Dialog" bind:dialog>
+  My dialog content is very, very long.<br />
+  It will not wrap by default.
+</Dialog>
