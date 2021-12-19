24ce4418436f14f583047acf47146c642cee7c2a
aifrak <aifrak@users.noreply.github.com>
2021-12-19 22:54:13 +0000

    Display error depending on status code from chapter 16.4

--- ch16/example-sapper/src/routes/_error.svelte
+++ ch16/example-sapper/src/routes/_error.svelte
@@ -1,4 +1,7 @@
 <script>
+  import { stores } from '@sapper/app';
+  const { page } = stores();
+
   export let status;
   export let error;
 
@@ -13,8 +16,14 @@
 
 <p>{error.message}</p>
 
-{#if dev && error.stack}
-  <pre>{error.stack}</pre>
+{#if status === 404}
+  There is no page mapped to {$page.path}.
+{:else}
+  <h1>Error: {error.message}</h1>
+  <p>status: {status}</p>
+  {#if dev && error.stack}
+    <pre>{error.stack}</pre>
+  {/if}
 {/if}
 
 <style>
@@ -22,14 +31,17 @@
   p {
     margin: 0 auto;
   }
+
   h1 {
     font-size: 2.8em;
     font-weight: 700;
     margin: 0 0 0.5em 0;
   }
+
   p {
     margin: 1em auto;
   }
+
   @media (min-width: 480px) {
     h1 {
       font-size: 4em;
