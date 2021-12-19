beae41e61b5e5691c8b1ab851465d00945754a3e
aifrak <aifrak@users.noreply.github.com>
2021-12-19 22:47:20 +0000

    Update layout depending on page and add background to blog page from chapter 16.3

--- ch16/example-sapper/src/routes/_layout.svelte
+++ ch16/example-sapper/src/routes/_layout.svelte
@@ -1,22 +1,35 @@
 <script>
+  import { stores } from '@sapper/app';
   import Nav from '../components/Nav.svelte';
 
+  // This prop is automatically passed to all layouts.
   export let segment;
+
+  const { page } = stores();
 </script>
 
-<Nav {segment} />
-
-<main>
+{#if $page.path === '/'}
+  <Nav {segment} />
   <slot />
-</main>
+{:else}
+  <Nav {segment} />
+  <main>
+    <section>
+      <slot />
+    </section>
+  </main>
+{/if}
 
 <style>
   main {
-    position: relative;
-    max-width: 56em;
-    background-color: white;
-    padding: 2em;
-    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+
+    background-color: linen;
     box-sizing: border-box;
+    /* The CSS variable --nav-height can be defined in static/global.css. */
+    height: calc(100vh - var(--nav-height));
+    padding: 2em;
+    width: 100vw;
   }
 </style>
---
+++ ch16/example-sapper/src/routes/blog/_layout.svelte
@@ -0,0 +1,11 @@
+<div>
+  <p>It's blog time!</p>
+  <slot />
+</div>
+
+<style>
+  div {
+    background-color: lightblue;
+    padding: 0.2rem 1rem;
+  }
+</style>
